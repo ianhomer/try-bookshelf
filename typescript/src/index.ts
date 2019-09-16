bcf5a1b75b0496d6bc73db34e1a60b13fcc7e4bd
Ian Homer <ihomer@worth.systems>
2019-09-16 08:05:36 +0100

    Typescript AOK

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -3,13 +3,24 @@ console.log("Trying Bookshelf with Typescript")
 import User from './entity/User'
 import Group from './entity/Group'
 
-User.forge({'name':'my-name'})
+const seed = Math.floor(new Date().getTime() / 1000)
+
+User.forge({'name':'my-name=' + seed})
     .save()
     .then(user => {
-        Group.forge({'name':'my-group'})
+        Group.forge({'name':'my-group-' + seed})
             .save()
             .then(group => {
                 console.log(`Saved group : ${JSON.stringify(user)}`)
+                try {
+                    console.log(`Attaching group`)
+                    user.groups().attach(group)
+                        .then(collection => `Saved relation ${collection}`)
+                        .catch(err => console.log(err))
+                    console.log(`Attached group`)
+                } catch (e) {
+                    console.log(e)
+                }
             })
             .catch(err => console.log(err))
         console.log('saved user')
